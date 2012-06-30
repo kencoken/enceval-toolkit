2b26a9bbc727c31dd203fac051dd4cb70dff47d8
Karen Simonyan <karen@robots.ox.ac.uk>
2012-06-30 19:14:45 +0100

    Added random initialisation for GMM

--- +featpipem/+codebkgen/@GMMCodebkGen/GMMCodebkGen.m
+++ +featpipem/+codebkgen/@GMMCodebkGen/GMMCodebkGen.m
@@ -6,16 +6,19 @@ classdef GMMCodebkGen < handle & featpipem.codebkgen.GenericCodebkGen
         descount_limit % limit on # features to use for clustering
         trainimage_limit % limit on # images to use for clustering  
         maxcomps % maximum number of comparisons when using ANN (-1 = exact)
+        GMM_init % GMM initialisation method
     end
     
     methods
         function obj = GMMCodebkGen(featextr, cluster_count)
             obj.featextr = featextr;
             obj.cluster_count = cluster_count;
-            obj.descount_limit = 10e5;
+            obj.descount_limit = 1e6;
             obj.trainimage_limit = -1;    
             obj.maxcomps = ceil(cluster_count/4);
+            obj.GMM_init = 'rand';
         end
+        
         codebook = train(obj, imlist, varargin)
     end
     
--- +featpipem/+codebkgen/@GMMCodebkGen/train.m
+++ +featpipem/+codebkgen/@GMMCodebkGen/train.m
@@ -67,43 +67,56 @@ end
 % 2. Cluster codebook centres
 % -------------------------------------------------------------------------
 
-fprintf('Computing initial means using K-means...\n');
+if isequal(obj.GMM_init, 'kmeans')
+    
+    fprintf('Computing initial means using K-means...\n');
 
-% if maxcomps is below 1, then use exact kmeans, else use approximate
-% kmeans with maxcomps number of comparisons for distances
-if obj.maxcomps < 1
-    init_mean = vl_kmeans(feats, obj.cluster_count, ...
-        'verbose', 'algorithm', 'elkan');
-else
-    init_mean = featpipem.lib.annkmeans(feats, obj.cluster_count, ...
-        'verbose', false, 'MaxNumComparisons', obj.maxcomps, ...
-        'MaxNumIterations', 150);
-end
+    % if maxcomps is below 1, then use exact kmeans, else use approximate
+    % kmeans with maxcomps number of comparisons for distances
+    if obj.maxcomps < 1
+        init_mean = vl_kmeans(feats, obj.cluster_count, ...
+            'verbose', 'algorithm', 'elkan');
+    else
+        init_mean = featpipem.lib.annkmeans(feats, obj.cluster_count, ...
+            'verbose', false, 'MaxNumComparisons', obj.maxcomps, ...
+            'MaxNumIterations', 150);
+    end
+    
+    fprintf('Computing initial variances and coefficients...\n');
 
-fprintf('Computing initial variances and coefficients...\n');
+    % compute hard assignments
+    kd_tree = vl_kdtreebuild(init_mean, 'numTrees', 3) ;
+    assign = vl_kdtreequery(kd_tree, init_mean, feats);
 
-% compute hard assignments
-kd_tree = vl_kdtreebuild(init_mean, 'numTrees', 3) ;
-assign = vl_kdtreequery(kd_tree, init_mean, feats);
+    % mixing coefficients
+    init_coef = single(vl_binsum(zeros(obj.cluster_count, 1), 1, double(assign)));
+    init_coef = init_coef / sum(init_coef);
 
-% mixing coefficients
-init_coef = single(vl_binsum(zeros(obj.cluster_count, 1), 1, double(assign)));
-init_coef = init_coef / sum(init_coef);
+    % variances
+    init_var = zeros(size(feats, 1), obj.cluster_count, 'single');
 
-% variances
-init_var = zeros(size(feats, 1), obj.cluster_count, 'single');
-
-for i = 1:obj.cluster_count
-    feats_cluster = feats(:, assign == i);
-    init_var(:, i) = var(feats_cluster, 0, 2);
+    for i = 1:obj.cluster_count
+        feats_cluster = feats(:, assign == i);
+        init_var(:, i) = var(feats_cluster, 0, 2);
+    end
+    
+elseif isequal(obj.GMM_init, 'rand')
+    init_mean = [];
+    init_var = [];
+    init_coef = [];
 end
 
 fprintf('Clustering features using GMM...\n');
 
+% call FMM mex
 gmm_params = struct;
-codebook = mexGmmTrainSP(feats, obj.cluster_count, gmm_params, init_mean, init_var, init_coef);
+
+if ~isempty(init_mean) && ~isempty(init_var) && ~isempty(init_coef)
+    codebook = mexGmmTrainSP(feats, obj.cluster_count, gmm_params, init_mean, init_var, init_coef);
+else
+    codebook = mexGmmTrainSP(feats, obj.cluster_count, gmm_params);
+end
 
 fprintf('Done training codebook!\n');
 
 end
-
